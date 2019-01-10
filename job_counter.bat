2d0ee3ffe6602401377f0ca24ab2a5e328a83333
Luke P <underage.loli.desu@gmail.com>
2019-01-10 17:29:24 +0000

    Add files via upload

--- job_counter.bat
+++ job_counter.bat
@@ -22,23 +22,53 @@ set /p _t1done=
 if %_t1done%==1 goto t1done
 goto skipt1
 :t1done
-
+(
+echo 0
+)>threads\t1.txt
 
 :skipt1
+
 
 rem thread 2
 (
 set /p _t2done=
 )<threads\t2.txt
+if %_t2done%==1 goto t2done
+goto skipt2
+:t2done
+(
+echo 0
+)>threads\t2.txt
+
+:skipt2
+
 
 rem thread 3
 (
 set /p _t3done=
 )<threads\t3.txt
+if %_t3done%==1 goto t3done
+goto skipt3
+:t3done
+(
+echo 0
+)>threads\t3.txt
+
+:skipt3
+
 
 rem thread 4
 (
 set /p _t4done=
 )<threads\t4.txt
+if %_t4done%==1 goto t4done
+goto skipt4
+:t4done
+(
+echo 0
+)>threads\t4.txt
+
+:skipt4
+
 
 goto main
